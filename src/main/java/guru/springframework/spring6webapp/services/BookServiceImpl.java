412ae84d20aa19554b7f60f2aa510112e1aef9d8
VladimirBabin <vladimir.sol.rojo@gmail.com>
2023-11-11 18:57:15 +0200

    books and authors views; BookController; AuthorController.

--- src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
+++ src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
-
     private final BookRepository bookRepository;
-
     public BookServiceImpl(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
